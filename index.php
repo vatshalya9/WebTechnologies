a9a06a6bae255b14191a2110c08c4e560ce4a2d4
vatshalya9 <vatshalya.d009@gmail.com>
2017-05-11 11:41:36 -0400

    Add files via upload
    ajax-registration-script-with-php-mysql-and-jquery

--- index.php
+++ index.php
@@ -1,135 +1,45 @@
-<html>
-<head>
-<title> </title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-<style>
-.live{
-	display:inline;
-}
-</style>
-</head>
-<body>
-<section style="width:400px;">
-<div style="background:#ddd;color:#000000;text-align:center;padding:5px;">
-<h4>Ajax</h4>
-<button class="ajax_button">Click here</button>
-</div>
-<div style="border:1px solid #000000;height:150px;">
-<p><b>Ajax states:</b></p>
-<b >Respose_text</b>: <p class="live" id="Respose_text"></p><br/>
-<b >readystate</b>: <p  class="live" id="readystate"></p><br/>
-<b >status</b>: <p  class="live" id="status"></p>
-</div>
-<div style="border:1px solid #000000;height:200px;color:#777;">
-<p><b>when 200 state:(Output)</b></p>
-<table border='1' >
-<thead>
-	<th>sno</th>
-	<th>response_text</th>
-	<th>ready status</th>
-	<th>status</th>
-</thead>
-<tbody id="table_success">
+<?php 
+include('header.php');
+include_once("db_connect.php");
+?>
+<title>phpzag.com : Demo Ajax Registration Script with PHP, MySQL and jQuery</title>
+<script type="text/javascript" src="script/validation.min.js"></script>
+<script type="text/javascript" src="script/register.js"></script>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+<?php include('container.php');?>
+
+<div class="container">
+	<h2>Example: Ajax Registration Script with PHP, MySQL and jQuery</h2>		
 	
-</tbody>
-</table>
-
-<p>note: here sno value is value of sno in database</p>
-</div>
-
-</section>
-<script>
-$(".ajax_button").click(function(){
-
-$.ajax({
-    url: "ajax.php",
-    type: "POST",
-    success: function(textStatus, xhr,msg) {
-        console.log(arguments);
-        console.log(xhr.status);
-		//alert(textStatus);
-		if(textStatus!=""){
-			$("#Respose_text").text(textStatus);
-			msg = textStatus;
-			response_text(msg);
-		}
+	<div class="register_container">
+	<form class="form-signin" method="post" id="register-form">
+	<h2 class="form-signin-heading">User Registration Form</h2><hr />
+	<div id="error">
+	</div>
+	<div class="form-group">
+	<input type="text" class="form-control" placeholder="Username" name="user_name" id="user_name" />
+	</div>
+	<div class="form-group">
+	<input type="email" class="form-control" placeholder="Email address" name="user_email" id="user_email" />
+	<span id="check-e"></span>
+	</div>
+	<div class="form-group">
+	<input type="password" class="form-control" placeholder="Password" name="password" id="password" />
+	</div>
+	<div class="form-group">
+	<input type="password" class="form-control" placeholder="Retype Password" name="cpassword" id="cpassword" />
+	</div>
+	<hr />
+	<div class="form-group">
+	<button type="submit" class="btn btn-default" name="btn-save" id="btn-submit">
+	<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account
+	</button> 
+	</div>  
+	</form>
+	</div>
 		
-    },
-    complete: function(xhr, textStatus) {
-        console.log(xhr.status);
-		xhr.onreadystatechange =  xhr.readyState;
-		//alert(xhr.onreadystatechange);
-		 if (xhr.readyState == 4 )
-    {
-		$("#readystate").text(xhr.readyState);
-	 ready_state(xhr.readyState);
-    }	
-	if (xhr.status == 200)
-    {
-		$("#status").text(xhr.status);
-       status_success(xhr.status);
-    }
-	}
-		
-	
-});	
-});
-function response_text(text){
-	form_data = "text="+text+"&state="+0+"&success="+0;
- $.ajax({
-        url: "insert_db.php",
-        type: "post",
-        data: form_data ,
-        success: function () {
-                        
-
-        },
-        error: function() {
-           
-        }
-
-
-    });
-	
-}
-function ready_state(state){
-	form_data = "text="+0+"&state="+state+"&success="+0;
- $.ajax({
-        url: "insert_db.php",
-        type: "post",
-        data: form_data ,
-        success: function () {
-                     
-
-        },
-        error: function() {
-          
-        }
-
-
-    });
-	
-}
-function status_success(status){
-	form_data = "text="+0+"&state="+0+"&success="+status;
- $.ajax({
-        url: "insert_db.php",
-        type: "post",
-        data: form_data ,
-        success: function (response) {
-               if(response!=""){
-				   $("#table_success").html(response);
-			   }  
-
-        },
-        error: function() {
-           
-        }
-
-
-    });
-	
-}
-</script>
-</body>
-</html>
+	<div style="margin:50px 0px 0px 0px;">
+		<a class="btn btn-default read-more" style="background:#3399ff;color:white" href="http://www.phpzag.com/ajax-registration-script-with-php-mysql-and-jquery" title="">Back to Tutorial</a>			
+	</div>		
+</div>
+<?php include('footer.php');?>
